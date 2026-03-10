89e5103998e589cd87fc465d536cc3eee23cff21
deepikasrivadlakonda-alt <deepikasrivadlakonda@gmail.com>
2026-03-10 15:19:35 +0000

    Random dice

--- Game work.py
+++ Game work.py
@@ -1,9 +1,12 @@
-def roll_dice():
-    return randint(1, 6)
+from random import randint
 
 
 def generate_target_number(lower_limit=17, upper_limit=29):
     return randint(lower_limit, upper_limit)
+
+
+def roll_dice():
+    return randint(1, 6)
 
 
 if __name__ == '__main__':
@@ -13,3 +16,20 @@ if __name__ == '__main__':
     print()
 
     total = 0
+
+    while True:
+        this_roll = roll_dice()
+        total += this_roll
+
+        print(f'You rolled {this_roll}. Your total is {total}.')
+
+        again = input('Would you like to roll again? (y/n): ')
+
+        if again == 'n':
+            break
+    score = abs(total - target_number)
+
+    if score <= 5:
+        print(f'Your score is {score}.')
+    else:
+        print('You lost!')
